6833205d55919b4bf67ab86b7a7e21df88a28f9a
nathan choi <nath49ers@gmail.com>
2024-02-17 14:20:42 -0800

    add convex functions

--- front-end/convex/findings.js
+++ front-end/convex/findings.js
@@ -2,9 +2,7 @@ import { query } from "./_generated/server"
 import { v } from "convex/values"
 
 export const get = query({
-  args: { _id: v.id("food_halls"), name: v.string() },
   handler: async (ctx) => {
-    console.log(ctx.db)
     return await ctx.db.query("food_halls").collect()
   }
 })
