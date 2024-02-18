d8b2246c80d0dd63d281cd92adee27db1823436b
nathan choi <nath49ers@gmail.com>
2024-02-18 02:36:55 -0800

    stuff

--- front-end/src/components/views/datatable-demo.jsx
+++ front-end/src/components/views/datatable-demo.jsx
@@ -1,31 +1,36 @@
 "use client"
-import * as React from "react"
+import { api } from '@/../convex/_generated/api';
 import {
-  CaretSortIcon,
-  ChevronDownIcon,
-  DotsHorizontalIcon
-} from "@radix-ui/react-icons"
+  ChevronDownIcon
+} from "@radix-ui/react-icons";
+
+import { Checkbox } from "@/components/ui/checkbox";
+
 import {
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
-  getPaginationRowModel,
   getSortedRowModel,
   useReactTable
-} from "@tanstack/react-table"
+} from "@tanstack/react-table";
+import { useQuery } from 'convex/react';
+import React from 'react';
 
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { DropdownMenuCheckboxItem } from "@/components/ui/dropdown-menu";
+import { ArrowUpDown } from "@radix-ui/react-icons"
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -35,102 +40,62 @@ import {
   TableRow
 } from "@/components/ui/table"
 
-const data = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com"
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com"
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com"
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com"
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com"
-  }
-]
 
 export const columns = [
   {
     id: "select",
     header: ({ table }) => (
       <Checkbox
+        className="translate-y-[2px]"
         checked={
           table.getIsAllPageRowsSelected() ||
           (table.getIsSomePageRowsSelected() && "indeterminate")
         }
-        onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
         aria-label="Select all"
       />
     ),
     cell: ({ row }) => (
       <Checkbox
+        className="translate-y-[2px]"
         checked={row.getIsSelected()}
-        onCheckedChange={value => row.toggleSelected(!!value)}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
         aria-label="Select row"
       />
     ),
     enableSorting: false,
-    enableHiding: false
+    enableHiding: false,
   },
   {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
-    )
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      )
-    },
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>
-  },
-  {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
+    accessorKey: "name",
+    header: () => <div className="text-left">Name</div>,
     cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"))
-
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-      }).format(amount)
-
-      return <div className="text-right font-medium">{formatted}</div>
+      return <div className="text-left font-medium">{row.getValue("name")}</div>
+    }
+  },
+  {
+    accessorKey: "city",
+    header: () => <div className="text-left">City</div>,
+    cell: ({ row }) => {
+      return <div className="text-left">{row.getValue("city")}</div>
+    }
+  },
+  {
+    accessorKey: "median_income",
+    header: () => <div className="text-left">Median income</div>,
+    cell: ({ row }) => {
+      return <div className="text-left">{row.getValue("median_income")}</div>
+    }
+  },
+  {
+    accessorKey: "population_density",
+    header: () => <div className="text-left">Population density</div>,
+    cell: ({ row }) => {
+      return <div className="text-left">{row.getValue("population_density")}</div>
     }
   },
   {
     id: "actions",
-    enableHiding: false,
     cell: ({ row }) => {
       const payment = row.original
 
@@ -139,7 +104,7 @@ export const columns = [
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="h-8 w-8 p-0">
               <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
+              <MoreHorizontal className="h-4 w-4" />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
@@ -165,13 +130,14 @@ export function DataTableDemo() {
   const [columnVisibility, setColumnVisibility] = React.useState({})
   const [rowSelection, setRowSelection] = React.useState({})
 
+  const data = useQuery(api.findings.getAll)
+
   const table = useReactTable({
     data,
     columns,
     onSortingChange: setSorting,
     onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
@@ -184,14 +150,18 @@ export function DataTableDemo() {
     }
   })
 
+  if (!table || !data) {
+    return <p>Loading</p>
+  }
+
   return (
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={table.getColumn("email")?.getFilterValue() ?? ""}
+          placeholder="Vector match..."
+          value={table.getColumn("name")?.getFilterValue() ?? ""}
           onChange={event =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
+            table.getColumn("name")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
@@ -220,7 +190,7 @@ export function DataTableDemo() {
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-      <div className="rounded-md border">
+      <div className="rounded-md border bg-white">
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map(headerGroup => (
@@ -231,9 +201,9 @@ export function DataTableDemo() {
                       {header.isPlaceholder
                         ? null
                         : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
                     </TableHead>
                   )
                 })}
@@ -241,32 +211,33 @@ export function DataTableDemo() {
             ))}
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map(row => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map(cell => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
+            {
+              table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map(row => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map(cell => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
+              )}
           </TableBody>
         </Table>
       </div>
--- test.py
+++ test.py
@@ -1,7 +1,28 @@
-from selenium import webdriver
+import pandas as pd
+import numpy as np
 
+csv = pd.read_csv('he.csv', header=0)
 
-driver = webdriver.Chrome()
+# iterate through the rows of the csv file
+for index, row in csv.iterrows():
+    print("{")
 
-driver.get("https://google.com")
-driver.get("https://google.com/images")
+    if row['Name'] and not pd.isna(row["Name"]):
+        print(f"\"name\": \"{row['Name']}\",")
+
+    if row['City'] and not pd.isna(row["City"]):
+        print(f"\"city\": \"{row['City']}\",")
+
+    if row['# of food stalls'] and not pd.isna(row["# of food stalls"]):
+        print(f"\"food\": \"{row['# of food stalls']}\",")
+
+    if row['Bars'] and not pd.isna(row["Bars"]):
+        print(f"\"bars\": \"{row['Bars']}\",")
+
+    if row['Retail'] and not pd.isna(row["Retail"]):
+        print(f"\"retail\": \"{row['Retail']}\",")
+
+    if row["Year Opened"] and not pd.isna(row["Year Opened"]):
+        print(f"\"year_established\": \"{row['Year Opened']}\",")
+
+    print("},")
